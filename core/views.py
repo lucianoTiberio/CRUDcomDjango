b3f5560095f5641ffa682d7a84758b86b825fa8a
tiberio <lucianotib2014@gmail.com>
2022-09-30 12:57:02 -0300

    Initial commit

---
+++ core/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render, redirect
+from .models import Pessoa
+
+# Create your views here.
+
+def home(request):
+    pessoas = Pessoa.objects.all()
+    return render(request, "index.html",{"pessoas": pessoas})
+
+def salvar(request):
+    vnome = request.POST.get("nome")
+    Pessoa.objects.create(nome=vnome)
+    pessoas = Pessoa.objects.all()
+    return render(request,"index.html", {"pessoas": pessoas})
+
+def editar(request,id):
+    pessoa = Pessoa.objects.get(id=id)
+    return render(request, "update.html", ({"pessoa":pessoa}))
+
+def update(request,id):
+    vnome = request.POST.get("nome")
+    pessoa = Pessoa.objects.get(id=id)
+    pessoa.nome = vnome
+    pessoa.save()
+    return redirect(home)
+
+def delete(request,id):
+    pessoa = Pessoa.objects.get(id=id)
+    pessoa.delete()
+    return redirect(home)
